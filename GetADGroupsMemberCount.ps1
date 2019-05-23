05df38c4da394e65aec969e3a267ced1eb99629b
Erud <edward.rudakov@gmail.com>
2019-05-23 16:51:50 -0500

    group with more then 500 members

---
+++ GetADGroupsMemberCount.ps1
@@ -0,0 +1,10 @@
+﻿$results = @()
+$groups = Get-ADGroup -Filter * -SearchBase "OU=Medline Industries,DC=medline,DC=Com"   
+foreach ($group in $groups) {
+	#$members = $group | Get-ADGroupMember
+	$ldapFilter = '(&(objectclass=user)(objectcategory=person)(memberof:1.2.840.113556.1.4.1941:={0}))' -f $group.DistinguishedName
+	$members = Get-ADObject -LDAPFilter $ldapFilter -SearchBase "DC=medline,DC=Com" -ResultSetSize $null -ResultPageSize 1000 -Properties:@('samAccountName')
+	$results += New-Object psObject -Property @{'GroupName'=$group;'Members'=$members.count}
+	$members = $null
+}
+$results | Export-Csv C:\Temp\MLgroups1.csv -NoTypeInformation
