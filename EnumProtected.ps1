2f1e2045f385d45f914ee9913ef61152f9030507
Erud <edward.rudakov@gmail.com>
2019-08-06 16:08:22 -0500

    Added for screen

---
+++ EnumProtected.ps1
@@ -0,0 +1,18 @@
+﻿# enum protected users groups contacts ou
+Get-ADUser -Filter {Name -like "*"} -Properties ProtectedFromAccidentalDeletion | where {$_.ProtectedFromAccidentalDeletion -eq $true} |
+Export-Csv C:\Temp/users_protected.csv -NoTypeInformation 
+
+Get-ADGroup -Filter {Name -like "*"} -Properties ProtectedFromAccidentalDeletion | where {$_.ProtectedFromAccidentalDeletion -eq $true} |
+Export-Csv C:\Temp/groups_protected.csv -NoTypeInformation 
+
+Get-ADObject -LDAPFilter "(objectClass=contact)" -Properties ProtectedFromAccidentalDeletion | where {$_.ProtectedFromAccidentalDeletion -eq $true} |
+Export-Csv C:\Temp/contacts_protected.csv -NoTypeInformation 
+
+Get-ADObject -LDAPFilter "(objectClass=organizationalUnit)" -Properties ProtectedFromAccidentalDeletion | where {$_.ProtectedFromAccidentalDeletion -eq $true} |
+Export-Csv C:\Temp/OUs_protected.csv -NoTypeInformation
+
+Get-ADObject -LDAPFilter "(objectClass=container)" -Properties ProtectedFromAccidentalDeletion | where {$_.ProtectedFromAccidentalDeletion -eq $true} |
+Export-Csv C:\Temp/containers_protected.csv -NoTypeInformation
+
+Get-ADComputer -Filter {Name -like "*"} -Properties ProtectedFromAccidentalDeletion | where {$_.ProtectedFromAccidentalDeletion -eq $true} |
+Export-Csv C:\Temp/computers_protected.csv -NoTypeInformation
---
+++ SimpleWindow.ps1
@@ -0,0 +1,13 @@
+﻿Add-Type -AssemblyName System.Windows.Forms
+$form = New-Object Windows.Forms.Form
+$form.Size = New-Object Drawing.Size @(200,100)
+$form.StartPosition = "CenterScreen"
+$Icon = New-Object system.drawing.icon ("C:\Users\erudakov\Documents\PS\favicon.ico")
+$form.Icon = $Icon
+$btn = New-Object System.Windows.Forms.Button
+$btn.add_click({Get-Date|Out-Host})
+$btn.Text = "Click here"
+$form.Controls.Add($btn)
+$drc = $form.ShowDialog()
+
+$Form.Dispose()
