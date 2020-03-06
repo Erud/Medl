b750eec76e4bab0f3d7fea50175966fe90e04472
Erud <edward.rudakov@gmail.com>
2020-03-06 15:20:13 -0600

    3/6

--- CopyFolderToK.ps1
+++ CopyFolderToK.ps1
@@ -1,19 +1,23 @@
 ﻿$name = "W1"
 $computers = get-content "c:\temp\$name.txt"
+
+$password = get-content C:\temp\cred.txt | convertto-securestring
+$Cred = new-object -typename System.Management.Automation.PSCredential -argumentlist "medline-nt\pa-erudakov",$password
+
 foreach ($comp in $computers) {
 	$comp
 	if (Test-Path k:) {
 		Remove-PSDrive -Name "K" -Force
 	}
 	
-	$password = get-content C:\temp\cred.txt | convertto-securestring
-	$Cred = new-object -typename System.Management.Automation.PSCredential -argumentlist "medline-nt\pa-erudakov",$password
-	
 	New-PSDrive -Name K -PSProvider FileSystem -Root "\\$comp\c$" -Persist -Credential $cred -Scope Global
 	if (Test-Path 'K:\Temp\WinCollectInstall') {
 		Remove-Item 'K:\Temp\WinCollectInstall' -Recurse
 	}
-	Copy-Item -Path "C:\Temp\WinCollectInstall" -Destination "K:\Temp\" -Recurse
+	if (-Not (Test-Path 'K:\Temp')) {
+		New-Item -Path "K:\" -Name "Temp" -ItemType "directory"
+	}
+	Copy-Item -Path "C:\Temp\WinCollectInstall\*" -Destination "K:\Temp\" -Recurse
 	
 	Remove-PSDrive -Name "K" -Force
 }
--- RunAsProcess.ps1
+++ RunAsProcess.ps1
@@ -1,10 +1,13 @@
 ﻿#Get UserB credential
 $Credential = Get-Credential medline-nt\pa-erudakov
 
+$mmc = "$($env:SystemDrive)\Windows\System32\mmc.exe"
+$msc = "$($env:SystemDrive)\Windows\System32\dsa.msc"
+
 #Use System.Diagnostics to start the process as UserB
 $ProcessInfo = New-Object System.Diagnostics.ProcessStartInfo
 #With FileName we're basically telling powershell to run another powershell process
-$ProcessInfo.FileName = "mmc.exe"
+$ProcessInfo.FileName = $mmc
 #CreateNoWindow helps avoiding a second window to appear whilst the process runs
 $ProcessInfo.CreateNoWindow = $true
 #Note the line below contains the Working Directory where the script will start from
@@ -13,11 +16,12 @@ $ProcessInfo.RedirectStandardError = $true
 $ProcessInfo.RedirectStandardOutput = $true 
 $ProcessInfo.UseShellExecute = $false
 #The line below is basically the command you want to run and it's passed as text, as an argument
-#$ProcessInfo.Arguments = "%SystemRoot%\system32\dsa.msc"
+#$ProcessInfo.Arguments = $msc
 #The next 3 lines are the credential for UserB, as you can see, we can't just pass $Credential
 $ProcessInfo.Username = $Credential.GetNetworkCredential().username
 $ProcessInfo.Domain = $Credential.GetNetworkCredential().Domain
 $ProcessInfo.Password = $Credential.Password
+$ProcessInfo.Verb = "RunAs"
 #Finally start the process and wait for it to finish
 $Process = New-Object System.Diagnostics.Process 
 $Process.StartInfo = $ProcessInfo 
